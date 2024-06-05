ccbdb497a5cc8aab7a16d9d5bc4f89c84afc6d10
Ankush Chander <ankush.watchtower@gmail.com>
2024-06-05 18:11:33 +0530

    add llm support

--- main.ts
+++ main.ts
@@ -1,82 +1,275 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	SuggestModal,
+	Plugin,
+	PluginSettingTab, Setting
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+import OpenAI from 'openai';
 
-interface MyPluginSettings {
+
+interface TextMoverPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface TextMoverPluginSettings {
+	mySetting: string;
+	useLLM: boolean;
+	openAIapiKey: string;
+	modelName: string;
+	maxTokens: number;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: TextMoverPluginSettings = {
+	mySetting: "default",
+	useLLM: false,
+	openAIapiKey: "",
+	modelName: "gpt-3.5-turbo",
+	maxTokens: 20,
+
+};
+
+
+interface Heading {
+	heading: string;
+	level: number;
+	position: object;
+}
+
+
+export class HeadingSuggestionModal extends SuggestModal<Heading> {
+	// Returns all available suggestions.
+	headings: Heading[];
+	result: object
+	onSubmit: (result: object) => void;
+
+	constructor(app: App, headings: Heading[], onSubmit: (result: object) => void) {
+		super(app);
+		console.log("inside constructor");
+		this.headings = headings;
+		// console.log(this.headings);
+		this.onSubmit = onSubmit;
+
+	}
+
+	onOpen() {
+		// console.log("inside onOpen");
+		super.onOpen();
+	}
+
+	getSuggestions(query: string): Heading[] {
+		return this.headings.filter((item) =>
+			item.heading.toLowerCase().includes(query.toLowerCase())
+		);
+	}
+
+	// Renders each suggestion item.
+	renderSuggestion(heading: Heading, el: HTMLElement) {
+		el.createEl("div", {text: heading.heading});
+	}
+
+	// Perform action on the selected suggestion.
+	onChooseSuggestion(heading: Heading, evt: MouseEvent | KeyboardEvent) {
+		this.onSubmit(heading);
+	}
+}
+
+
+export default class TextMoverPlugin extends Plugin {
+	settings: TextMoverPluginSettings;
+	llm_client: OpenAI;
 
 	async onload() {
 		await this.loadSettings();
+		await this.build_api()
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Move text to heading")
+						.setIcon("document")
+						.onClick(async () => {
+							let selection = editor.getSelection();
+							const cursorposition = editor.getCursor()
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+							if (selection == "") {
+								// get clicked text
+								selection = editor.getLine(cursorposition.line)
+							}
+							console.log(selection)
+							// TODO: get cursor position
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+							const file = this.app.workspace.getActiveFile()
+							// get headings from file
+							if (!file) {
+								return
+							}
+							const filecache = this.app.metadataCache.getFileCache(file)
+							let headings: Heading[] = [];
+							if (filecache && filecache.headings) {
+								headings = filecache.headings.map(headingCache => {
+									return {
+										heading: headingCache.heading,
+										level: headingCache.level,
+										position: headingCache.position
+									};
+								});
+							}
+							if (this.settings.useLLM) {
+								const heading_str = headings.map(heading => heading.heading).join(", ")
+								const prompt = "For the text below, suggest top3 classes from one of the following classes(no fluff, no explaination, no numbering just the classes): \n" +
+									"```\n " +
+									"classes:" + heading_str + "\n" +
+									"text: " + selection
+								// api post call to openai
+								const chatCompletion = await this.llm_client.chat.completions.create({
+									messages: [{role: 'user', content: prompt}],
+									model: this.settings.modelName,
+								});
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+								// get chosen classes
+								let chosen_classses: string[] = [];
+								const llm_response = chatCompletion.choices[0].message.content
+								// alert notice the class
+								// split classes by ", " or newline
+								if (llm_response == null || llm_response.length==0){
+									chosen_classses = []
+								}
+								else if (llm_response.includes("\n")) {
+									chosen_classses = llm_response.split("\n")
+								} else if (llm_response.includes(", ")) {
+									chosen_classses = llm_response.split(", ")
+								} else if (llm_response.length > 0) {
+									chosen_classses = llm_response.split(", ")
+								}
+								// strip non alphabetic characters
+								chosen_classses = chosen_classses.map((item: string) => item.trim().replace(/[^a-zA-Z /]/g, ''))
+								console.log(chosen_classses);
+								//sort heading by chosen_classes
+								headings.sort((a, b) => {
+									return -(chosen_classses.indexOf(a.heading) - chosen_classses.indexOf(b.heading));
+								})
+							}
+							const hmodal = new HeadingSuggestionModal(this.app, headings, (result) => {
+								const cursorposition = editor.getCursor()
+								// concatenate all headings with ,
+								// @ts-ignore
+								const targetPosition = {"line": result.position.end.line, "ch": result.position.end.ch}
+								// insert selection under heading								  
+								editor.replaceRange(`\n${selection}`, targetPosition)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+								const start = {"line": cursorposition.line, "ch": 0}
+								const end = {"line": cursorposition.line, "ch": selection.length + 1}
+								editor.replaceRange("", start, end)
+							});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+							hmodal.open()
+						});
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+				})
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			}))
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Auto classify")
+						.setIcon("document")
+						.onClick(async () => {
+
+							const file = this.app.workspace.getActiveFile()
+							// get headings from file
+							if (!file) {
+								return
+							}
+							const filecache = this.app.metadataCache.getFileCache(file)
+							let headings: Heading[] = [];
+							if (filecache && filecache.headings) {
+								headings = filecache.headings.map(headingCache => {
+									return {
+										heading: headingCache.heading,
+										level: headingCache.level,
+										position: headingCache.position
+									};
+								});
+							}
+							if (typeof (headings) == "undefined" || headings == null || headings.length == 0) {
+								return
+							}
+							// @ts-ignore
+							const lastLine: number = headings.last().position.end.line
+							// generate difference array
+							const line_heading_map: number[] = Array.apply(0, Array(lastLine)).map(Number.prototype.valueOf, 0)
+
+							console.log(line_heading_map)
+
+							// console.log(lastLine)
+							for (let i = 1, prev_heading = 0; i < headings.length; i++) {
+								// @ts-ignore
+								line_heading_map[headings[i].position.end.line] = headings[i].position.end.line - prev_heading
+								// @ts-ignore
+								prev_heading = headings[i].position.end.line
+							}
+							console.log(line_heading_map)
+							// generate actual array from difference array
+							const actual_line_heading_map: number[] = []
+							for (let i = 0; i < line_heading_map.length; i++) {
+								if (i == 0) {
+									actual_line_heading_map[i] = line_heading_map[i]
+								} else {
+									actual_line_heading_map[i] = actual_line_heading_map[i - 1] + line_heading_map[i]
+								}
+							}
+
+							console.log(actual_line_heading_map)
+							let training_instances: object[] = []
+
+							actual_line_heading_map.map((ele, idx: number) => {
+								let input = editor.getLine(idx)
+								// replace numerical prefixes from input with ""
+								input = input.replace(/\d+\./g, "")
+								input = input.replace(/#+ /g, "")
+
+								// remove trailing whitespace
+								input = input.trim()
+								let target = editor.getLine(ele)
+								// replace preceeding # and spaces with ""
+
+								target = target.replace(/#+ /g, "")
+								training_instances.push({
+									"input": input,
+									"target": target
+								})
+							})
+							// filter training examples with empty input or target
+							// @ts-ignore
+							training_instances = training_instances.filter(ele => ele.input != "" && ele.target != "" && ele.input != ele.target)
+							console.log(training_instances)
+							// for (let i = 0; i < actual_line_heading_map.length; i++) {
+							// 	const line_text = editor.getLine(i)
+							//
+							// 	if (line_text.trim() == "") {
+							// 		continue;
+							// 	}
+							// 	console.log(line_text)
+							// 	const heading_line = actual_line_heading_map[i]
+							// 	console.log(editor.getLine(heading_line))
+							// }
+
+
+							// total lines
+
+						});
+
+				})
+
+			}))
+
+		this.addSettingTab(new TextMoverSettingTab(this.app, this));
+
 	}
+
 
 	onunload() {
 
@@ -89,46 +282,71 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	async build_api() {
+		console.log("build api here.")
+		this.llm_client = new OpenAI({
+			apiKey: this.settings.openAIapiKey, // This is the default and can be omitted
+			dangerouslyAllowBrowser: true,
+		});
+		console.log(this.llm_client)
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+class TextMoverSettingTab extends PluginSettingTab {
+	plugin: TextMoverPlugin;
+
+	constructor(app: App, plugin: TextMoverPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
+		containerEl.createEl("h2", {text: "Settings for my TextMover plugin."});
+		const api_key_setting = new Setting(containerEl).setName("OpenAI API Key").addText((text) =>
+			text
+				.setPlaceholder("Enter OpenAI key here")
+				.setValue(this.plugin.settings.openAIapiKey)
+				.onChange(async (value) => {
+					this.plugin.settings.openAIapiKey = value;
+					await this.plugin.saveSettings();
+					this.plugin.build_api();
+				}),
+		);
+		const model_name_setting = new Setting(containerEl)
+			.setName("Model Name")
+			.setDesc("Select your model")
+			.addDropdown((dropdown) =>
+				dropdown
+					.addOptions({
+						"gpt-3.5-turbo": "gpt-3.5-turbo",
+						"gpt-4": "gpt-4",
+					})
+					.setValue(this.plugin.settings.modelName)
+					.onChange(async (value) => {
+						this.plugin.settings.modelName = value;
+						await this.plugin.saveSettings();
+						this.plugin.build_api();
+					}),
+			);
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Use LLM")
+			.setDesc("Select to get smart classification using LLM")
+			.addToggle((toogle) => {
+				toogle
+					.setValue(this.plugin.settings.useLLM)
+					.onChange(async (value) => {
+						this.plugin.settings.useLLM = value;
+						await this.plugin.saveSettings();
+						this.plugin.build_api();
+						api_key_setting.setDisabled(!value);
+						model_name_setting.setDisabled(!value);
+					});
+			});
 	}
 }
